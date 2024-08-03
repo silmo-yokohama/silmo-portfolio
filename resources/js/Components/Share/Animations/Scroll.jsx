d7056495e9abd3444277a5689dfb9d614edaabc6
silmo-yokohama <silmo.yokohama@gmail.com>
2024-08-03 19:35:50 +0900

    clean: リファクタ

--- resources/js/Components/Share/Animations/Scroll.jsx
+++ resources/js/Components/Share/Animations/Scroll.jsx
@@ -4,8 +4,8 @@ import "../../../../css/scroll-animation.css";
 const Scroll = () => {
   return (
     <div className="flex items-center flex-col mt-[76vh]">
-      <div class="p-scroll-mouse"></div>
-      <p class="p-scroll-text text-primary">Scroll</p>{" "}
+      <div className="p-scroll-mouse"></div>
+      <p className="p-scroll-text text-primary">Scroll</p>
     </div>
   );
 };
